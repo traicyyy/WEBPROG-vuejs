69d8e5c22d6bb9eccbb48895ed1f20978f53defa
Tracie Tomon <tctomon@student.apc.edu.ph>
2025-01-09 16:56:47 +0800

    Create shopping.js

---
+++ home/js/shopping.js
@@ -0,0 +1,29 @@
+const app3 = Vue.createApp({
+    data() {
+      return {
+        itemName: null,
+        itemNumber: null,
+        itemImportant: false,
+        shoppingList: [
+          { name: 'Tomatoes', number: 5, important: false, found: false },
+          { name: 'Bread', number: 1, important: false, found: false },
+          { name: 'Soap', number: 1, important: true, found: true }
+        ]
+      }
+    },
+    methods: {
+      addItem(){
+        let item = {
+          name: this.itemName,
+          number: this.itemNumber,
+          important: this.itemImportant,
+          found: false
+        }
+        this.shoppingList.push(item)
+        this.itemName = null
+        this.itemNumber = null  
+        this.itemImportant = false
+      }
+    }
+  })
+ app3.mount('#app3')
