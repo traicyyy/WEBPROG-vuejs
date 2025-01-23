e851bea2f154910227d916e0a3687a76de039fc1
Tracie Tomon <tctomon@student.apc.edu.ph>
2025-01-23 16:34:57 +0800

    updated food

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,18 +1,11 @@
 <template>
   <h1>Food</h1>
   <food-item/>
-  <food-item/>
+  <food-item2/>
   <food-item/>
 </template>
 
 <script></script>
 
 <style>
-  #app > div {
-        border: dashed black 1px;
-        display: inline-block;
-        margin: 10px;
-        padding: 10px;
-        background-color: orange;
-    }
 </style> 
--- vue-project/src/components/FoodItem2.vue
+++ vue-project/src/components/FoodItem2.vue
@@ -9,8 +9,8 @@
 export default {
   data() {
     return {
-      name: 'Oranges',
-      message: 'I like oranges'
+      name: 'Banana',
+      message: 'I like bananas'
     }
   }
 };
@@ -22,6 +22,6 @@ export default {
         display: inline-block;
         margin: 10px;
         padding: 10px;
-        background-color: orange;
+        background-color: yellow;
     }
 </style>
