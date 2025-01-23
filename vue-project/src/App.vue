47c8b333af6775b3b760a35edb83abb6751be5ce
Tracie Tomon <tctomon@student.apc.edu.ph>
2025-01-23 16:20:29 +0800

    Adding The Component

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,7 +1,18 @@
-<script></script>
- 
 <template>
-  <h1>Kon'nichiwa, Tracie here:)</h1>
+  <h1>Food</h1>
+  <food-item/>
+  <food-item/>
+  <food-item/>
 </template>
- 
-<style></style>
+
+<script></script>
+
+<style>
+  #app > div {
+        border: dashed black 1px;
+        display: inline-block;
+        margin: 10px;
+        padding: 10px;
+        background-color: orange;
+    }
+</style> 
---
+++ vue-project/src/components/FoodItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Oranges',
+      message: 'I like oranges'
+    }
+  }
+};
+</script>
+
+<style></style>
