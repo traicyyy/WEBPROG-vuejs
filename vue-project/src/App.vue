af21605585945b2c965757db678174b1b485ebed
Tracie Tomon <tctomon@student.apc.edu.ph>
2025-01-27 09:21:58 +0000

    Added Supabase

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,12 +1,31 @@
 <template>
-  <h1 style="color: white;">Food</h1>
+  <h1>Food</h1>
+  <ul>
+      <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
+  </ul>
+  <br>
   <food-item/>
   <food-item2/>
   <food-item/> <br>
   <personal-profile/> <br>
 </template>
 
-<script></script>
+
+<script setup>
+  import { ref, onMounted } from 'vue'
+  import { supabase } from './lib/supabaseClient'
+
+  const countries = ref([])
+
+  async function getCountries() {
+    const { data } = await supabase.from('countries').select()
+    countries.value = data
+  }
+
+  onMounted(() => {
+    getCountries()
+  })
+</script>
 
 <style>
 </style>           
---
+++ vue-project/src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://rscgkbthdrnsohrruqbj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzY2drYnRoZHJuc29ocnJ1cWJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5NjgyMTYsImV4cCI6MjA1MzU0NDIxNn0.84WByW9L8CQxl1zELMt_q3rlJJQIbnn63QvFFEta3vY')
