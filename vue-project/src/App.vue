ad0a8e9916e0b9c1b278cd1f9bfade91dacb2be3
Tracie Tomon <tctomon@student.apc.edu.ph>
2025-01-30 08:21:38 +0000

    update

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,15 +1,13 @@
 <template>
   <h1 style="color: white;">Food</h1>
-  <ul style="color: white;">
+  <ul style="color: white; list-style-type: disc;">
       <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
   </ul>
-  <br>
   <food-item/> 
   <food-item2/> 
   <food-item/> <br>
   <personal-profile/> <br>
 </template>
-
 
 <script setup>
   import { ref, onMounted } from 'vue'
