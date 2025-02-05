e7ddb41f1a5432fdd09eaf945d7c9b3a7f80e395
Tracie Tomon <tctomon@student.apc.edu.ph>
2025-02-06 02:19:42 +0800

    updated

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -13,13 +13,13 @@
 
 <style>
 .food-title {
-  font-family: "Proxima Nova Medium", sans-serif;
-  font-size: 36px;
-  text-align: center;
-  color: #db841c;
-  margin-top: 20px;
-  text-transform: uppercase;
-  letter-spacing: 2px;
-  font-weight: bold;
+    font-family: Proxima Nova Medium,sans-serif;
+    font-size: 36px;
+    padding-left: 20px;
+    color: #db841c;
+    margin-top: 20px;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    font-weight: 700
 }
 </style>
